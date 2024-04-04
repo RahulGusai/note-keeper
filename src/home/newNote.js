c0354d9403c71afb6f6a21d3b73fd8ed4d775fc6
Rahul Gusai <rgusai97@gmail.com>
2024-04-04 15:53:58 +0530

    Redesigning new note container

--- src/home/newNote.js
+++ src/home/newNote.js
@@ -1,11 +1,13 @@
 import './newNote.css';
 import { forwardRef, useEffect, useState } from 'react';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 function ComponentHandler(props, ref) {
   const { titleRef, contentRef } = ref;
   const { isExpanded, newNoteClickHandler } = props;
-  const [bulletEnabled, setBulletEnabled] = useState(false);
+  const [isDefaultTextLoaded, setisDefaultTextLoaded] = useState({
+    content: true,
+    title: true,
+  });
 
   function handleNewNoteClick(e) {
     if (!isExpanded) {
@@ -13,29 +15,38 @@ function ComponentHandler(props, ref) {
     }
   }
 
-  function insertBulletPoint() {
-    if (bulletEnabled) {
-      //TODO Handle true case
-      setBulletEnabled(false);
-    } else {
-      const currentContent = contentRef.current.innerHTML;
-      contentRef.current.innerHTML = `${currentContent}<p>&#8226;&nbsp;</p>`;
-      setBulletEnabled(true);
-    }
-  }
-
   useEffect(() => {
-    if (!isExpanded) {
-      contentRef.current.innerHTML =
-        'Take a note. You can click anywhere on the screen to save it';
-    } else {
-      contentRef.current.innerHTML = '';
+    if (isExpanded) {
+      contentRef.current.focus();
+      const range = document.createRange();
+      const selection = window.getSelection();
+      range.selectNodeContents(contentRef.current);
+      range.collapse(true);
+      selection.removeAllRanges();
+      selection.addRange(range);
     }
   }, [contentRef, isExpanded]);
 
+  const handleKeyPressedOnContent = (e) => {
+    if (isDefaultTextLoaded.content) {
+      contentRef.current.innerHTML = '';
+      setisDefaultTextLoaded((isDefaultTextLoaded) => {
+        return { ...isDefaultTextLoaded, content: false };
+      });
+    }
+  };
+
+  const handleKeyPressedOntTitle = (e) => {
+    if (isDefaultTextLoaded.title) {
+      titleRef.current.innerHTML = '';
+      setisDefaultTextLoaded((isDefaultTextLoaded) => {
+        return { ...isDefaultTextLoaded, title: false };
+      });
+    }
+  };
+
   const contentClassesName = `content ${isExpanded ? 'expanded' : 'default'}`;
   const titleClassesname = `title ${isExpanded ? 'show' : 'hide'}`;
-
   const footerClassesName = `footerContainer ${isExpanded ? 'show' : 'hide'}`;
 
   return (
@@ -47,28 +58,21 @@ function ComponentHandler(props, ref) {
       <div
         contentEditable
         ref={titleRef}
-        role="textbox"
         className={titleClassesname}
+        onKeyDown={handleKeyPressedOntTitle}
       >
         Title
       </div>
       <div
         contentEditable
         ref={contentRef}
-        role="textbox"
         className={contentClassesName}
-      ></div>
-
-      <div className={footerClassesName}>
-        <div className="icons">
-          <FormatListBulletedIcon
-            style={{ color: 'white' }}
-            onClick={insertBulletPoint}
-            className="bulletIcon"
-          ></FormatListBulletedIcon>
-        </div>
-        <div className="button">Close</div>
+        onKeyDown={handleKeyPressedOnContent}
+      >
+        Take a note...
       </div>
+
+      <div className={footerClassesName}></div>
     </div>
   );
 }
